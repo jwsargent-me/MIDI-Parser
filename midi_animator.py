a2867c45fa94cb8e5b1148cecc24b3c13e280e60
J Sargent <36310130+jwsargent-me@users.noreply.github.com>
2021-04-12 09:07:52 -0400

    updated midi-animator.py
    Minor updates to midi-animator.py which should help animation keyframes work as expected and make the file easier to use

--- midi_animator.py
+++ midi_animator.py
@@ -4,7 +4,11 @@ from pathlib import Path
 
 
 def set_blank_keys(n, fr):
+    # define animation target object
     obj = bpy.data.objects[str(n)]
+
+    obj.rotation_euler = (0, obj.rotation_euler[1], obj.rotation_euler[2])
+
     obj.keyframe_insert(data_path='rotation_euler', frame=fr)
     obj.keyframe_insert(data_path='color', frame=fr)
 
@@ -44,12 +48,10 @@ def note_off(note, value, frame, mult):
     set_anim_keys(n, frame + ease_frames, 0, velocity_multiplier, 0)
 
 
-# def pedal_change(v, t):
-#     frame = get_frame(t, ticks_per_frame)
-
-
 # Global variables
-path = Path(__file__).parent / "D:\Dropbox\PyCharm Projects\MIDI Parser\JSON\Angels We Have Heard On High b.json"
+file_name = ''
+path = 'D:/Dropbox/PyCharm Projects/MIDI Parser/JSON/' + file_name + '.json'
+path = Path(__file__).parent / path
 
 ease_multiplier = 1
 velocity_multiplier = 45
@@ -63,3 +65,12 @@ for e in data:
         note_on(data[e]['note'], data[e]['value'], data[e]['frame'], ease_multiplier)
     if data[e]['type'] == 'note_off':
         note_off(data[e]['note'], data[e]['value'], data[e]['frame'], ease_multiplier)
+
+'''
+Potential issues:
+ - It appears that midi notes may be slightly early, but consistently so
+ - Some notes may be getting dropped/garbled. Part of this is how blank frames are set, 
+    and part of this is how they are generated. When writing to frames, we lose a lot of data. 
+    Might be worth saving to 60fps and then rendering out every other frame
+ -  
+'''
